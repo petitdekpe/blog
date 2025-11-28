2976f175cb5eeeba9ab2cf755822a45376828b5b
petitdekpe <petitdekpe@gmail.com>
2025-11-28 22:12:46 +0100

    acd

--- src/Controller/Admin/ArticleCrudController.php
+++ src/Controller/Admin/ArticleCrudController.php
@@ -14,6 +14,11 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 
 class ArticleCrudController extends AbstractCrudController
 {
@@ -22,28 +27,61 @@ class ArticleCrudController extends AbstractCrudController
         return Article::class;
     }
 
+    public function createEntity(string $entityFqcn)
+    {
+        $article = new Article();
+        $article->setTags([]);
+        return $article;
+    }
+
     public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
     {
-        if ($entityInstance instanceof Article && is_string($entityInstance->getTags())) {
-            $tagsString = $entityInstance->getTags();
-            $tagsArray = array_map('trim', explode(',', $tagsString));
-            $entityInstance->setTags(array_filter($tagsArray));
+        if ($entityInstance instanceof Article) {
+            $request = $this->container->get('request_stack')->getCurrentRequest();
+            $tagsString = $request->request->all()['Article']['tagsInput'] ?? '';
+
+            if (!empty($tagsString)) {
+                $tagsArray = array_map('trim', explode(',', $tagsString));
+                $entityInstance->setTags(array_filter($tagsArray));
+            } else {
+                $entityInstance->setTags([]);
+            }
         }
         parent::persistEntity($entityManager, $entityInstance);
     }
 
     public function updateEntity(EntityManagerInterface $entityManager, $entityInstance): void
     {
-        if ($entityInstance instanceof Article && is_string($entityInstance->getTags())) {
-            $tagsString = $entityInstance->getTags();
-            $tagsArray = array_map('trim', explode(',', $tagsString));
-            $entityInstance->setTags(array_filter($tagsArray));
+        if ($entityInstance instanceof Article) {
+            $request = $this->container->get('request_stack')->getCurrentRequest();
+            $tagsString = $request->request->all()['Article']['tagsInput'] ?? '';
+
+            if (!empty($tagsString)) {
+                $tagsArray = array_map('trim', explode(',', $tagsString));
+                $entityInstance->setTags(array_filter($tagsArray));
+            } else {
+                $entityInstance->setTags([]);
+            }
         }
         parent::updateEntity($entityManager, $entityInstance);
     }
 
     public function configureFields(string $pageName): iterable
     {
+        $context = $this->getContext();
+        $article = $context ? $context->getEntity()->getInstance() : null;
+        $tagsValue = '';
+
+        if ($article instanceof Article && $article->getTags()) {
+            $tagsValue = implode(', ', $article->getTags());
+        }
+
+        $tagsField = TextField::new('tagsInput', 'Tags')
+            ->setHelp('Entrez les tags séparés par des virgules (Ex: Article, À la une, Sport)')
+            ->setFormTypeOption('mapped', false)
+            ->setFormTypeOption('data', $tagsValue)
+            ->onlyOnForms();
+
         return [
             IdField::new('id')->hideOnForm(),
             TextField::new('title', 'Titre'),
@@ -59,11 +97,15 @@ class ArticleCrudController extends AbstractCrudController
                     return implode(', ', $names);
                 }),
             AssociationField::new('author', 'Auteur'),
-            TextareaField::new('tags', 'Tags')
-                ->setHelp('Entrez les tags séparés par des virgules (Ex: Article, À la une, Sport)')
-                ->formatValue(function ($value) {
-                    return is_array($value) ? implode(', ', $value) : '';
-                }),
+            $tagsField,
+            TextField::new('tagsDisplay', 'Tags')
+                ->formatValue(function ($value, $entity) {
+                    if ($entity instanceof Article && $entity->getTags()) {
+                        return implode(', ', $entity->getTags());
+                    }
+                    return '';
+                })
+                ->onlyOnIndex(),
             TextareaField::new('excerpt', 'Extrait')->hideOnIndex(),
             TextEditorField::new('content', 'Contenu')->hideOnIndex(),
             ImageField::new('image', 'Image')
